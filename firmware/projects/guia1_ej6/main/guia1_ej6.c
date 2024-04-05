0c1b6b1e0b2f833eaf0067e39a0f9ba35a62871d
facundomain <facundo.main@ingenieria.uner.edu.ar>
2024-04-05 11:41:47 -0300

    Se agrega ejercicio 6 listo

--- firmware/projects/guia1_ej6/main/guia1_ej6.c
+++ firmware/projects/guia1_ej6/main/guia1_ej6.c
@@ -84,8 +84,8 @@ void muestra_numero ( uint32_t pData, uint8_t pDigitos, gpioConf_t *gpios_digito
 	uint8_t bcd_number[3];
 	while (pData != 0)
 	{		
-		bcd_number[pDigitos] = pData % 10;  //456 % 10 = 6
-		pData = pData / 10;  // data ahora es 45
+		bcd_number[pDigitos] = pData % 10;  
+		pData = pData / 10;  
 		++pDigitos;
 	}
 
@@ -94,9 +94,9 @@ void muestra_numero ( uint32_t pData, uint8_t pDigitos, gpioConf_t *gpios_digito
 	gpios_digito[2].pin = GPIO_22;
 	gpios_digito[3].pin = GPIO_23;
 
-	seleccion_bcd[0].pin = GPIO_19;
-	seleccion_bcd[1].pin = GPIO_18;  //esto puede quedar alreves
-	seleccion_bcd[2].pin = GPIO_9;
+	seleccion_bcd[0].pin = GPIO_9;
+	seleccion_bcd[1].pin = GPIO_18; 
+	seleccion_bcd[2].pin = GPIO_19;
 
 	for (uint8_t i = 0 ; i < 3 ; ++i){
 		
@@ -112,7 +112,7 @@ void muestra_numero ( uint32_t pData, uint8_t pDigitos, gpioConf_t *gpios_digito
 /*==================[external functions definition]==========================*/
 void app_main(void){
 
-	uint32_t numero = 456;
+	uint32_t numero = 146;
 	uint8_t digitos = 0;
 	gpioConf_t gpios[4];
 	gpioConf_t seleccion_bcd[3];
