7bb5a8cee9234e262edf2f8c0cec7b98e07f025e
facundomain <facundo.main@ingenieria.uner.edu.ar>
2024-06-11 11:34:30 -0300

    Se crea folder del parcial

---
+++ firmware/projects/ParcialMAIN/main/ParcialMAIN.c
@@ -0,0 +1,39 @@
+/*! @mainpage Parcial Main
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 11/06/2024 | Document creation		                         |
+ *
+ * @author Facundo Main (facundo.main@ingenieria.uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
